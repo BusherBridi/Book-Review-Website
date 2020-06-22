cdee11f2fb1c6c3d0ff843a8bf36d3ad0cfabe84
Busher Bridi <bridibusher@gmail.com>
2020-06-22 15:53:18 -0700

    fixed login logic

--- application.py
+++ application.py
@@ -21,33 +21,41 @@ if not os.getenv("DATABASE_URL"):
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
-app.secret_key = ""
-
+app.secret_key = "verysecretkey"
 
 
 @app.route("/")
 def index():
     if ("logged_in" in session):
         if(session["logged_in"] == True):
-            return "good"
-        
+            return render_template("goToDashboard.html")
+        else:
+            return render_template("index.html")
     else:
         return render_template("index.html")
 
 
 @app.route("/dashboard", methods=["POST"])
 def dashboard():
-    username = request.form.get("username")
-    password = request.form.get("password")
-    if(db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 1):
-        session["logged_in"] = True
-        firstName = db.execute("SELECT firstName FROM users WHERE username = :username AND password = :password", {
-                               "username": username, "password": password}).fetchone()
-        firstName = (firstName.firstname).capitalize()
-        return render_template("userPage.html", firstName=firstName)
+    firstName = "User"
+    
+    if "user_info" in session and session["logged_in"] == True:
+            firstName = session["user_info"]
+            return render_template("userPage.html", firstName=firstName)
     else:
-        errorMessage = "username or password is incorrect"
-        return render_template("error.html", error=errorMessage)
+        username = request.form.get("username")
+        password = request.form.get("password")
+        if(db.execute("SELECT * FROM users WHERE username = :username AND password = :password", {"username": username, "password": password}).rowcount == 1 or session["logged_in"] == True):
+            session["logged_in"] = True
+            firstName = db.execute("SELECT firstName FROM users WHERE username = :username AND password = :password", {
+                                    "username": username, "password": password}).fetchone()
+            firstName = (firstName.firstname).capitalize()
+            session["user_info"] = firstName
+            return render_template("userPage.html", firstName = firstName)
+            
+        else:
+            errorMessage = "username or password is incorrect"
+            return render_template("error.html", error=errorMessage)
 
 
 @app.route("/signUp")
@@ -89,57 +97,57 @@ def searchResult():
     query = query.upper()
     queryParameter = request.form.get("searchGroup")
     results = None
-    
+
     if(queryParameter == "isbn"):
-        if(db.execute("SELECT * FROM books WHERE upper(isbn) LIKE CONCAT('%', :isbn, '%')",{"isbn":query}).rowcount == 0):
+        if(db.execute("SELECT * FROM books WHERE upper(isbn) LIKE CONCAT('%', :isbn, '%')", {"isbn": query}).rowcount == 0):
             hasResults = False
         else:
-            results = db.execute("SELECT * FROM books WHERE upper(isbn) LIKE CONCAT('%', :isbn, '%')",{"isbn":query}).fetchall()
+            results = db.execute(
+                "SELECT * FROM books WHERE upper(isbn) LIKE CONCAT('%', :isbn, '%')", {"isbn": query}).fetchall()
             hasResults = True
-        
+
     if(queryParameter == "author"):
-        if(db.execute("SELECT * FROM books WHERE upper(author) LIKE CONCAT('%', :author, '%')", {"author":query}).rowcount == 0):
+        if(db.execute("SELECT * FROM books WHERE upper(author) LIKE CONCAT('%', :author, '%')", {"author": query}).rowcount == 0):
             hasResults = False
         else:
-            results = db.execute("SELECT * FROM books WHERE upper(author) LIKE CONCAT('%', :author, '%')", {"author":query}).fetchall()
+            results = db.execute(
+                "SELECT * FROM books WHERE upper(author) LIKE CONCAT('%', :author, '%')", {"author": query}).fetchall()
             hasResults = True
     if(queryParameter == "title"):
-        if(db.execute("SELECT * FROM books WHERE upper(title) LIKE CONCAT('%', :title, '%')", {"title":query}).rowcount == 0):
+        if(db.execute("SELECT * FROM books WHERE upper(title) LIKE CONCAT('%', :title, '%')", {"title": query}).rowcount == 0):
             hasResults = False
-        else:    
-            results = db.execute("SELECT * FROM books WHERE upper(title) LIKE CONCAT('%', :title, '%')", {"title":query}).fetchall()
+        else:
+            results = db.execute(
+                "SELECT * FROM books WHERE upper(title) LIKE CONCAT('%', :title, '%')", {"title": query}).fetchall()
             hasResults = True
-    return render_template("searchResult.html", results = results, hasResults = hasResults)
-   
+    return render_template("searchResult.html", results=results, hasResults=hasResults)
 
-@app.route("/review/<string:isbn>", methods = ["POST","GET"])
+
+@app.route("/review/<string:isbn>", methods=["POST", "GET"])
 def displayInfo(isbn):
-    book = db.execute("SELECT * FROM books WHERE isbn = :isbn",{"isbn":isbn}).fetchone()
+    book = db.execute("SELECT * FROM books WHERE isbn = :isbn",
+                      {"isbn": isbn}).fetchone()
     session["book_id"] = book.id
     if request.method == "POST":
         session["book_id"] = book.id
-        return render_template("reviewPage.html", book = book)
+        return render_template("reviewPage.html", book=book)
     if request.method == "GET":
         if book is None:
             return jsonify({"error": "No book with ISBN in database"}), 422
         else:
             return jsonify({
-                "isbn":book.isbn,
-                "title":book.title,
-                "author":book.author
+                "isbn": book.isbn,
+                "title": book.title,
+                "author": book.author
             })
 
-@app.route("/review/confirm", methods = ["POST"])
+
+@app.route("/review/confirm", methods=["POST"])
 def confirm():
     rating = request.form.get("rating")
     review = request.form.get("review")
     b_id = session["book_id"]
-    #TODO: Add this stuff to the DB.. Somehow get the USER_ID and BOOK_ID to add this in the DB
-    #TODO: Add code to 'reviewPage.html' to display current reviews from DB
-    #TODO: Add confirm.html (and error?html) to tell user if DB was updated
+    # TODO: Add this stuff to the DB.. Somehow get the USER_ID and BOOK_ID to add this in the DB
+    # TODO: Add code to 'reviewPage.html' to display current reviews from DB
+    # TODO: Add confirm.html (and error?html) to tell user if DB was updated
     return (str(b_id))
-
-
-  
-
-
